de035532c601006d2c6a6b1e08fb330352e6eb72
hamzaJaral <hamzajarral62@gmail.com>
2025-03-14 00:40:46 +0500

    updated prod reqs

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -74,3 +74,4 @@ INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
 INSTALLED_APPS += ["django_extensions"]
+
