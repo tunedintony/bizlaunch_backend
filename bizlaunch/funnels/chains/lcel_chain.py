927ca1566513deafea1d29eb4e992dcba57ebac0
hamzaJaral <hamzajarral62@gmail.com>
2025-04-10 09:11:42 +0500

    changed the model from global var to function local var

--- bizlaunch/funnels/chains/lcel_chain.py
+++ bizlaunch/funnels/chains/lcel_chain.py
@@ -1,14 +1,15 @@
 from decouple import config
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
-from langchain_openai import ChatOpenAI
+from langchain_openai.chat_models import ChatOpenAI
 from pydantic import BaseModel
 
 from bizlaunch.funnels.chains.utils import build_prompt
 
-MODEL = ChatOpenAI(model="gpt-4o", temperature=0.2, api_key=config("OPENAI_API_KEY"))
-
 
 def create_ad_copy_chain(parser: BaseModel):
+    model = ChatOpenAI(
+        model="gpt-4o", temperature=0.2, api_key=config("OPENAI_API_KEY")
+    )
     # Get format instructions once during chain creation
     format_instructions = parser.get_format_instructions()
 
@@ -20,7 +21,7 @@ def create_ad_copy_chain(parser: BaseModel):
                 format_instructions,
             )
         )
-        | MODEL
+        | model
         | parser
     )
     return chain
